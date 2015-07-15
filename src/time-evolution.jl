8259befcf80babb76c9f22920d415a48d8aa6ede
Jim Garrison <jim@garrison.cc>
2015-07-15 14:52:03 -0700

    Refactor time evolution a bit

--- src/time-evolution.jl
+++ src/time-evolution.jl
@@ -4,45 +4,59 @@
 # constructing the original state in the energy basis, and again when
 # moving the time evolved states back to the position basis.
 
-function time_evolve(load_momentum_sector::Function, state_table::RepresentativeStateTable, initial_state::Vector, time_steps::Vector{Float64})
-    lattice_nmomenta = nmomenta(state_table.hs.lattice)
-
-    if length(initial_state) != length(state_table.hs.indexer)
+function to_energy_basis(load_momentum_sector::Function, state_table::RepresentativeStateTable, initial_states::Union(Vector,Matrix))
+    basis_size = length(state_table.hs.indexer)
+    if size(initial_states, 1) != basis_size
         throw(ArgumentError("Initial state must match indexer size"))
     end
+    nstates = size(initial_states, 2)
 
     ###
     # Transform initial state to energy basis
     ###
-    initial_energy_state = Complex128[]
+    initial_energy_states = Array(Complex128, size(initial_states)...)
+    offset = 0
     for sector_index in 1:state_table.sector_count
-        for momentum_index in 1:lattice_nmomenta
+        for momentum_index in 1:nmomenta(state_table.hs.lattice)
             reduced_indexer, reduced_energies, reduced_eigenstates = load_momentum_sector(sector_index, momentum_index)
             @assert length(reduced_indexer) == length(reduced_energies) == size(reduced_eigenstates, 1) == size(reduced_eigenstates, 2)
+            myrange = offset+1 : offset+length(reduced_indexer)
             diagsect = DiagonalizationSector(state_table, sector_index, momentum_index, reduced_indexer)
 
-            # Project onto current momentum basis
-            initial_momentum_state = zeros(Complex128, length(diagsect))
-            for (i, (reduced_i, alpha)) in enumerate(diagsect.representative_v)
-                if reduced_i != 0
-                    initial_momentum_state[reduced_i] += initial_state[i] * conj(alpha)
+            for x in 1:nstates
+                # Project onto current momentum basis
+                initial_momentum_state = zeros(Complex128, length(diagsect))
+                for (i, (reduced_i, alpha)) in enumerate(diagsect.representative_v)
+                    if reduced_i != 0
+                        initial_momentum_state[reduced_i] += initial_states[i,x] * conj(alpha)
+                    end
                 end
+
+                # Transform to energy basis
+                initial_energy_states[myrange,x] = reduced_eigenstates' * initial_momentum_state
             end
 
-            # Transform to energy basis
-            initial_energy_momentum_state = reduced_eigenstates' * initial_momentum_state
-            append!(initial_energy_state, initial_energy_momentum_state)
+            offset += length(reduced_indexer)
         end
     end
-    @assert length(initial_energy_state) == length(state_table.hs.indexer)
+    @assert offset == basis_size
+
+    return initial_energy_states
+end
+
+function time_evolve_to_position_basis(load_momentum_sector::Function, state_table::RepresentativeStateTable, initial_energy_state::Vector, time_steps::Vector{Float64})
+    basis_size = length(state_table.hs.indexer)
+    if size(initial_energy_state, 1) != basis_size
+        throw(ArgumentError("Initial energy state must match indexer size"))
+    end
 
     ###
     # Time evolve and move back to position basis
     ###
-    output_states = zeros(Complex128, length(initial_state), length(time_steps))
+    output_states = zeros(Complex128, basis_size, length(time_steps))
     offset = 0
     for sector_index in 1:state_table.sector_count
-        for momentum_index in 1:lattice_nmomenta
+        for momentum_index in 1:nmomenta(state_table.hs.lattice)
             reduced_indexer, reduced_energies, reduced_eigenstates = load_momentum_sector(sector_index, momentum_index)
             @assert length(reduced_indexer) == length(reduced_energies) == size(reduced_eigenstates, 1) == size(reduced_eigenstates, 2)
             myrange = offset+1 : offset+length(reduced_indexer)
@@ -64,7 +78,10 @@ function time_evolve(load_momentum_sector::Function, state_table::Representative
             offset += length(reduced_indexer)
         end
     end
-    @assert(offset == length(initial_state))
+    @assert offset == basis_size
 
     return output_states
 end
+
+time_evolve(load_momentum_sector::Function, state_table::RepresentativeStateTable, initial_state::Vector, time_steps::Vector{Float64}) =
+    time_evolve_to_position_basis(load_momentum_sector, state_table, to_energy_basis(load_momentum_sector, state_table, initial_state), time_steps)

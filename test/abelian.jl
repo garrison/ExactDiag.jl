417e263c84e0b2fdea601ee9039baf2a57d5a2ae
Jim Garrison <jim@garrison.cc>
2015-07-25 20:35:00 -0700

    Test all momentum sectors

--- test/abelian.jl
+++ test/abelian.jl
@@ -13,30 +13,36 @@ let
 
     full_ham = operator_matrix(hs, apply_hamiltonian)
 
-    # FIXME: test other sectors too, especially other momentum sectors
-    diagsect = DiagonalizationSector(rst, 1, 1)
-    reduced_ham = construct_reduced_hamiltonian(diagsect)
-    evals, evecs = eigs(reduced_ham, which=:SR)
-    for i in 1:length(evals)
-        eval = real(evals[i])
-        evec = evecs[:,i]
-        full_evec = get_full_psi(diagsect, evec)
+    processed_length = 0
+    for k_idx in 1:nmomenta(lattice)
+        diagsect = DiagonalizationSector(rst, 1, k_idx)
+        processed_length += length(diagsect)
+        reduced_ham = construct_reduced_hamiltonian(diagsect)
+        evals, evecs = eigs(reduced_ham, which=:SR)
+        for i in 1:length(evals)
+            eval = real(evals[i])
+            evec = evecs[:,i]
+            full_evec = get_full_psi(diagsect, evec)
 
-        @test_approx_eq_eps eigenstate_badness(full_ham, eval, full_evec) 0 1e-8
-        @test_approx_eq_eps eigenstate_badness(hs, apply_hamiltonian, eval, full_evec) 0 1e-8
-        check_eigenstate(full_ham, eval, full_evec)
-        @test_throws InexactError check_eigenstate(full_ham, eval + 1, full_evec)
+            @test_approx_eq_eps eigenstate_badness(full_ham, eval, full_evec) 0 1e-8
+            @test_approx_eq_eps eigenstate_badness(hs, apply_hamiltonian, eval, full_evec) 0 1e-8
+            check_eigenstate(full_ham, eval, full_evec)
+            @test_throws InexactError check_eigenstate(full_ham, eval + 1, full_evec)
 
-        if i == 1
-            L = length(lattice)
-            ψ = get_full_psi(diagsect, evec)
-            for L_A in 0:div(L, 2)
-                ent_cut1 = entanglement_entropy(Tracer(hs, 1:L_A), ψ)
-                ent_cut2 = entanglement_entropy(Tracer(hs, 1:L-L_A), ψ)
-                # FIXME: test against known results
-                @test_approx_eq_eps ent_cut1 ent_cut2 1e-8
+            if i == 1
+                L = length(lattice)
+                ψ = get_full_psi(diagsect, evec)
+                for L_A in 0:div(L, 2)
+                    ent_cut1 = entanglement_entropy(Tracer(hs, 1:L_A), ψ)
+                    ent_cut2 = entanglement_entropy(Tracer(hs, 1:L-L_A), ψ)
+                    # FIXME: test against known results
+                    @test_approx_eq_eps ent_cut1 ent_cut2 1e-8
+                end
             end
         end
+        if k_idx == 1
+            @test_approx_eq evals[1] -3.651093408937176
+        end
     end
-    @test_approx_eq evals[1] -3.651093408937176
+    @test processed_length == length(indexer)
 end

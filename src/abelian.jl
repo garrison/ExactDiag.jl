e356b09e031e1fe5c00c815543229732013cf1bb
Jim Garrison <jim@garrison.cc>
2015-08-07 21:12:16 -0700

    Temporary workaround for JuliaLang/julia#12512
    This improves benchmarks dramatically for e.g. the L=12 Hubbard model

--- src/abelian.jl
+++ src/abelian.jl
@@ -136,7 +136,7 @@ immutable RepresentativeStateTable{HilbertSpaceType<:HilbertSpace}
 
             # We've discovered a new sector!
             sector_count += 1
-            hamiltonian_basis_queue = Set{Int}()
+            hamiltonian_basis_queue = IndexedArray{Int}()
             push!(hamiltonian_basis_queue, z)
 
             while !isempty(hamiltonian_basis_queue)
@@ -201,7 +201,7 @@ immutable RepresentativeStateTable{HilbertSpaceType<:HilbertSpace}
                 apply_hamiltonian(hs, y) do newidx, amplitude
                     if amplitude != 0
                         if newidx > length(state_info_v) || state_info_v[newidx].representative_index == 0
-                            push!(hamiltonian_basis_queue, newidx)
+                            findfirst!(hamiltonian_basis_queue, newidx)
                         end
                     end
                 end

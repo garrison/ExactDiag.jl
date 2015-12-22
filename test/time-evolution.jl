garrison/ExactDiag.jl

commit f0ddf4ce3a5e597bf2c4cd36793aba0fb7dd3aba

Allow doing time evolution on a subset of k_indices
needs tests

src/time-evolution.jl
@@ -58,7 +58,7 @@ end
 my_Ac_mul_B(a, b) = Ac_mul_B(a, b)
 my_A_mul_B!(c, a, b) = A_mul_B!(c, a, b)
 
-function to_energy_basis(load_momentum_sector::Function, state_table::RepresentativeStateTable, initial_states::VecOrMat)
+function to_energy_basis(load_momentum_sector::Function, state_table::RepresentativeStateTable, initial_states::VecOrMat; k_indices=1:nmomenta(state_table.hs.lattice))
     basis_size = length(state_table.hs.indexer)
     if size(initial_states, 1) != basis_size
         throw(ArgumentError("Initial state must match indexer size"))
@@ -73,7 +73,7 @@ function to_energy_basis(load_momentum_sector::Function, state_table::Representa
     offset = 0
     initial_momentum_state = Complex128[]
     for sector_index in 1:state_table.sector_count
-        for momentum_index in 1:nmomenta(state_table.hs.lattice)
+        for momentum_index in k_indices
             reduced_indexer, reduced_energies, reduced_eigenstates = load_momentum_sector(sector_index, momentum_index)
             @assert length(reduced_indexer) == length(reduced_energies) == size(reduced_eigenstates, 1) == size(reduced_eigenstates, 2)
             myrange = offset+1 : offset+length(reduced_indexer)
@@ -108,7 +108,7 @@ function to_energy_basis(load_momentum_sector::Function, state_table::Representa
     return initial_energy_states, all_energies
 end
 
-function time_evolve_to_position_basis{TimeType<:Real}(load_momentum_sector::Function, state_table::RepresentativeStateTable, initial_energy_states::VecOrMat, time_steps::AbstractVector{TimeType})
+function time_evolve_to_position_basis{TimeType<:Real}(load_momentum_sector::Function, state_table::RepresentativeStateTable, initial_energy_states::VecOrMat, time_steps::AbstractVector{TimeType}; k_indices=1:nmomenta(state_table.hs.lattice))
     basis_size = length(state_table.hs.indexer)
     if size(initial_energy_states, 1) != basis_size
         throw(ArgumentError("Initial energy state must match indexer size"))
@@ -120,7 +120,7 @@ function time_evolve_to_position_basis{TimeType<:Real}(load_momentum_sector::Fun
     output_states = zeros(Complex128, size(initial_energy_states, 1), length(time_steps), size(initial_energy_states)[2:end]...)
     offset = 0
     for sector_index in 1:state_table.sector_count
-        for momentum_index in 1:nmomenta(state_table.hs.lattice)
+        for momentum_index in k_indices
             reduced_indexer, reduced_energies, reduced_eigenstates = load_momentum_sector(sector_index, momentum_index)
             @assert length(reduced_indexer) == length(reduced_energies) == size(reduced_eigenstates, 1) == size(reduced_eigenstates, 2)
             diagsect = DiagonalizationSector(state_table, sector_index, momentum_index, reduced_indexer)
@@ -160,7 +160,7 @@ function time_evolve_to_position_basis{TimeType<:Real}(load_momentum_sector::Fun
     return output_states
 end
 
-function time_evolve(load_momentum_sector::Function, state_table::RepresentativeStateTable, initial_states::VecOrMat, time_steps::AbstractVector{Float64})
-    ψ_e, = to_energy_basis(load_momentum_sector, state_table, initial_states)
-    return time_evolve_to_position_basis(load_momentum_sector, state_table, ψ_e, time_steps)
+function time_evolve(load_momentum_sector::Function, state_table::RepresentativeStateTable, initial_states::VecOrMat, time_steps::AbstractVector{Float64}; kwargs...)
+    ψ_e, = to_energy_basis(load_momentum_sector, state_table, initial_states; kwargs...)
+    return time_evolve_to_position_basis(load_momentum_sector, state_table, ψ_e, time_steps; kwargs...)
 end

commit 77292df2a86b7f1c803da5424415b29e11f397bd

Fix time evolution with a subset of k_indices
Has been broken since f0ddf4ce3a5e597bf2c4cd36793aba0fb7dd3aba

src/time-evolution.jl
@@ -58,18 +58,33 @@ end
 my_Ac_mul_B(a, b) = Ac_mul_B(a, b)
 my_A_mul_B!(c, a, b) = A_mul_B!(c, a, b)
 
+function calculate_energy_basis_size(state_table::RepresentativeStateTable, k_indices)
+    if k_indices == 1:nmomenta(state_table.hs.lattice)
+        return length(state_table.hs.indexer)
+    end
+
+    energy_basis_size = 0
+    for sector_index in 1:state_table.sector_count
+        for momentum_index in k_indices
+            energy_basis_size += length(DiagonalizationSector(state_table, sector_index, momentum_index))
+        end
+    end
+    return energy_basis_size
+end
+
 function to_energy_basis(load_momentum_sector::Function, state_table::RepresentativeStateTable, initial_states::VecOrMat; k_indices=1:nmomenta(state_table.hs.lattice))
-    basis_size = length(state_table.hs.indexer)
-    if size(initial_states, 1) != basis_size
+    if size(initial_states, 1) != length(state_table.hs.indexer)
         throw(ArgumentError("Initial state must match indexer size"))
     end
     nstates = size(initial_states, 2)
 
+    energy_basis_size = calculate_energy_basis_size(state_table, k_indices)
+
     ###
     # Transform initial state to energy basis
     ###
-    initial_energy_states = Array(Complex128, size(initial_states)...)
-    all_energies = sizehint!(Float64[], basis_size)
+    initial_energy_states = Array(Complex128, energy_basis_size, size(initial_states)[2:end]...)
+    all_energies = sizehint!(Float64[], energy_basis_size)
     offset = 0
     initial_momentum_state = Complex128[]
     for sector_index in 1:state_table.sector_count
@@ -105,21 +120,22 @@ function to_energy_basis(load_momentum_sector::Function, state_table::Representa
             end
         end
     end
-    @assert offset == basis_size
+    @assert offset == energy_basis_size
 
     return initial_energy_states, all_energies
 end
 
 function time_evolve_to_position_basis{TimeType<:Real}(load_momentum_sector::Function, state_table::RepresentativeStateTable, initial_energy_states::VecOrMat, time_steps::AbstractVector{TimeType}; k_indices=1:nmomenta(state_table.hs.lattice))
-    basis_size = length(state_table.hs.indexer)
-    if size(initial_energy_states, 1) != basis_size
+    energy_basis_size = calculate_energy_basis_size(state_table, k_indices)
+    if size(initial_energy_states, 1) != energy_basis_size
         throw(ArgumentError("Initial energy state must match indexer size"))
     end
 
     ###
     # Time evolve and move back to position basis
     ###
-    output_states = zeros(Complex128, size(initial_energy_states, 1), length(time_steps), size(initial_energy_states)[2:end]...)
+    basis_size = length(state_table.hs.indexer)
+    output_states = zeros(Complex128, basis_size, length(time_steps), size(initial_energy_states)[2:end]...)
     offset = 0
     for sector_index in 1:state_table.sector_count
         for momentum_index in k_indices
@@ -159,7 +175,7 @@ function time_evolve_to_position_basis{TimeType<:Real}(load_momentum_sector::Fun
             end
         end
     end
-    @assert offset == basis_size
+    @assert offset == energy_basis_size
 
     return output_states
 end

test/time-evolution.jl
@@ -91,4 +91,37 @@ let
             @test_approx_eq output_states2[:, :, 2] (im * output_states)
         end
     end
+
+    # Try evolving a state whose support is only on a subset of momentum sectors
+    initial_state = zeros(Complex128, length(rst.hs.indexer))
+    initial_state[findfirst(hs.indexer, [0,1,0,1,0,1,0,1])] = 1
+
+    let output_states = time_evolve(calculate_momentum_sector, rst, initial_state, time_steps, k_indices=[1,5])
+        # Test the output is of the correct size
+        @test size(output_states) == (length(rst.hs.indexer), length(time_steps))
+
+        # Test that time-evolved norms are all (essentially) unity
+        for t_i in 1:length(time_steps)
+            @test_approx_eq norm(output_states[:, t_i]) 1
+        end
+
+        # Test that evolving for zero time returns that same wavefunction
+        @test_approx_eq initial_state output_states[:, end]
+
+        # Test against standard evolution
+        let output_states2 = time_evolve(calculate_momentum_sector, rst, initial_state, time_steps)
+            @test_approx_eq output_states2 output_states
+        end
+    end
+
+    # Try providing an incomplete subset of momentum sectors
+    let
+        ψ_e, = ExactDiag.to_energy_basis(calculate_momentum_sector, rst, initial_state, k_indices=[1])
+
+        # Test that the vector is the correct size
+        @test length(ψ_e) < length(rst.hs.indexer)
+
+        # Test that the norm is not unity.
+        @test_approx_eq norm(ψ_e) 1/sqrt(2)
+    end
 end
6efe20779864e4811c036baa61ea3fa0c52d930e
Jim Garrison <jim@garrison.cc>
2017-04-25 12:49:50 -0400

    Use testset's

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,14 +22,9 @@ debug = false
 @test ExactDiag.exp_2πiη(1//3) ≈ exp(2π/3 * im)
 @test ExactDiag.exp_2πiη(1//3) ≈ exp(2π/3 * im)
 
-macro myinclude(filename)
-    debug && println(filename)
-    include(filename)
-end
-
-@myinclude "spin_half.jl"
-@myinclude "hubbard.jl"
-@myinclude "abelian.jl"
-@myinclude "abelian-spinflip.jl"
-@myinclude "abelian-reflection.jl"
-@myinclude "time-evolution.jl"
+@testset "Spin half         " begin include("spin_half.jl") end
+@testset "Hubbard           " begin include("hubbard.jl") end
+@testset "Abelian           " begin include("abelian.jl") end
+@testset "Abelian spin-flip " begin include("abelian-spinflip.jl") end
+@testset "Abelian reflection" begin include("abelian-reflection.jl") end
+@testset "Time evolution    " begin include("time-evolution.jl") end
